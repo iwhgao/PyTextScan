0dcbdba82f3486f78a46f98634378ed387b82619
WenhuiGao <gaowenhui2009@aliyun.com>
2015-11-15 23:54:49 +0800

    2015-11-15

---
+++ PyTextMask.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+# -*- coding: utf8 -*-
+'''
+利用Python脚本对文本文件的内容进行敏感信息识别与脱敏处理
+'''
+
+import os
+import sys
+import jieba
+
+def nameMask():
+    '''对中文姓名进行识别与处理'''
+    pass
+
+def bankCardMask():
+    '''对银行卡号进行识别与处理'''
+    pass
